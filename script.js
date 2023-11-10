7cd71d8074a40912f7195fcf274b42d6d27a5aaa
leliezer17 <leliezer123@gmail.com>
2023-11-10 12:37:04 -0400

    modificacion

---
+++ script.js
@@ -0,0 +1,15 @@
+let datosString = "hola mundo"
+let datoNumber  = 45
+let datoBooleano = false
+let datoVancio = null
+let datoIndefinido = undefined
+let datoLista = []
+let datoObjeto = {
+    username: "" ,
+    password: "" ,
+    age: "" ,
+    email: "" ,
+}
+
+console.log (datoNumber)
+console.log(datoObjeto)
